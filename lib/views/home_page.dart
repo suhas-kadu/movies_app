4c65a22990a2176821ff566f987684fa2356dac5
suhas-kadu <kadusuhas72@gmail.com>
2021-09-14 00:46:25 +0530

    basic layout: HomePage

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -3,8 +3,70 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        drawer: Drawer(),
+        appBar: AppBar(
+          elevation: 0,
+          title: Text("India"),
+          centerTitle: true,
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(50),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: TabBar(
+                indicatorSize: TabBarIndicatorSize.label,
+                isScrollable: true,
+                tabs: [
+                  Tab(
+                    text: "New",
+                  ),
+                  Tab(
+                    text: "Popular",
+                  ),
+                  Tab(
+                    text: "Upcoming",
+                  ),
+                ],
+              ),
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(Icons.search),
+            )
+          ],
+        ),
+        body: TabBarView(
+          children: [
+            Container(),
+            Container(),
+            Container(),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(currentIndex: 0, items: [
+          BottomNavigationBarItem(
+            label: "",
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.bookmark,
+            ),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+            ),
+            label: "",
+          )
+        ]),
+      ),
     );
   }
 }

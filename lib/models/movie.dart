9bb2dcc4f1d2cf7e7e1b039ebe371ffb76cd9230
suhas-kadu <kadusuhas72@gmail.com>
2021-09-14 10:20:48 +0530

    data fetched through model, style: Movie widget

---
+++ lib/models/movie.dart
@@ -0,0 +1,8 @@
+class Movie {
+  final String title;
+  final String imgUrl;
+  final String genre;
+  final String rating;
+
+  Movie({required this.title, required this.imgUrl, required this.genre, required this.rating});
+}
